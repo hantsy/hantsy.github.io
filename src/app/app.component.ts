b6d3b2f170c31954a6d2f7da4ec01f25d726de18
hantsy <hantsy@gmail.com>
2026-07-31 07:53:02 +0800

    Add global loading bar on route navigation; disable font inlining

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,48 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { RouterOutlet, Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { HeaderComponent } from './components/header.component';
 import { FooterComponent } from './components/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, MatProgressBarModule, HeaderComponent, FooterComponent],
   template: `
+    @if (loading()) {
+      <mat-progress-bar mode="indeterminate" class="global-loader" />
+    }
     <app-header />
     <main class="container">
       <router-outlet />
     </main>
     <app-footer />
   `,
+  styles: [`
+    .global-loader {
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      z-index: 9999;
+    }
+  `],
 })
-export class AppComponent {}
+export class AppComponent {
+  private router = inject(Router);
+  loading = signal(false);
+
+  constructor() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.loading.set(true);
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.loading.set(false);
+      }
+    });
+  }
+}
